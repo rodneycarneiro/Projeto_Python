f2439c16bec0de1c8d827ac347811e95a455e9ce
Rodney Carneiro <78662620+rodneycarneiro@users.noreply.github.com>
2022-04-17 10:07:40 -0300

    Primeiro programa em Python

---
+++ Hello.py
@@ -0,0 +1,7 @@
+# Author....: Rodney Carneiro
+# Data......: 16/04/2022
+# Objetivo..: Programa padrao como sendo o primeiro programa na Linguagem.
+
+#------ Programa Principal
+print("HELLO WORLD..")
+print()
